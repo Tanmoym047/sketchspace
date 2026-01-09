3deb5cb361094936596216fd3b126f0ac0126b8a
Tanmoym047 <tanmoym047@gmail.com>
2026-01-10 00:38:39 +0600

    unnecessary files removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Slider from './Slider';
 
 import Newsletter from './Newsletter';
-import RecentBlogs from './RecentBlogs/RecentBlogs';
 
 const Home = () => {
     return (
         <div className='mt-6'>
             <title>SketchSpace | Home</title>
             <Slider></Slider>
-            {/* <RecentBlogs></RecentBlogs> */}
             <Newsletter></Newsletter>
         </div>
     );
