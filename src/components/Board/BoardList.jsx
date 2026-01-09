3687f99b56d654a3c92adea22f3358a48e8fdc7a
Tanmoym047 <tanmoym047@gmail.com>
2026-01-10 03:43:40 +0600

    boardlist title changed

--- src/components/Board/BoardList.jsx
+++ src/components/Board/BoardList.jsx
@@ -60,6 +60,7 @@ const BoardList = () => {
 
     return (
         <div className="p-8 max-w-6xl mx-auto">
+            <title>SketchSpace | Boards </title>
             <div className="flex justify-between items-center mb-8">
                 <div>
                     <h1 className="text-2xl font-bold">Your Boards</h1>
